e8a873cced9bf94a74fcf626852c494aa7dadf4e
bartoszdabkowski <bartoszdabkowski@Bartoszs-MacBook-Pro.local>
2017-07-25 17:43:55 -0700

    upgraded to bootstrap 4 alpha

--- wwwroot/dist/main-client.js
+++ wwwroot/dist/main-client.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "349f253bcf4d5483f9c4"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "ee797b05ac85089d10b9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -2266,7 +2266,7 @@ module.exports = XmlEntities;
 /* 27 */
 /***/ (function(module, exports) {
 
-module.exports = "<div class='container-fluid'>\r\n     <nav-bar></nav-bar> \r\n    <div class=\"app-body\">\r\n        <div class='row'>\r\n            <div class='col-sm-2'>\r\n                <nav-menu></nav-menu>\r\n            </div>\r\n            <div class='col-sm-10'>  \r\n                <div class=\"app-margin-top\">\r\n                    <router-outlet></router-outlet>\r\n                </div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>\r\n";
+module.exports = "<div class='container-fluid'>\r\n      <nav-bar></nav-bar>  \r\n    <div class=\"app-body\">\r\n        <div class='row'>\r\n            <div class='col-sm-2'>\r\n                <nav-menu></nav-menu>\r\n            </div>\r\n            <div class='col-sm-10'>  \r\n                <div class=\"app-margin-top\">\r\n                    <router-outlet></router-outlet>\r\n                </div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>\r\n";
 
 /***/ }),
 /* 28 */
@@ -4039,7 +4039,7 @@ exports.push([module.i, ".nav-bar{\n    padding-bottom: 75px;\n}\n\n.btn-custom{
 /* 55 */
 /***/ (function(module, exports) {
 
-module.exports = "\n<nav class=\"navbar navbar-inverse navbar-fixed-top\">\n  <div class=\"container-fluid\">\n    <!-- Brand and toggle get grouped for better mobile display -->\n    <div class=\"navbar-header\">\n      <button type=\"button\" class=\"btn btn-custom navbar-btn pull-left nav-hide\">\n        <span class=\"glyphicon glyphicon-menu-hamburger\" aria-hidden=\"true\"></span>\n      </button>\n      <a class=\"navbar-brand\" href=\"#\">Find-A-Fish</a>\n    </div>\n  </div><!-- /.container-fluid -->\n</nav>\n";
+module.exports = "\n<nav class=\"navbar navbar-inverse navbar-fixed-top\">\n  <div class=\"container-fluid\">\n    <!-- Brand and toggle get grouped for better mobile display -->\n    <div class=\"navbar-header\">\n      <button type=\"button\" class=\"btn btn-custom navbar-btn pull-left nav-hide\">\n        <span class=\"glyphicon glyphicon-menu-hamburger\" aria-hidden=\"true\"></span>\n      </button>\n      \n      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">\n        <span class=\"sr-only\">Toggle navigation</span>\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n      </button>\n      <a class=\"navbar-brand\" href=\"#\">Find-A-Fish</a>\n    </div>\n\n    <!-- Collect the nav links, forms, and other content for toggling -->\n    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">\n      <ul class=\"nav navbar-nav\">\n        <li class=\"active\"><a href=\"#\">Link <span class=\"sr-only\">(current)</span></a></li>\n        <li><a href=\"#\">Link</a></li>\n        <li class=\"dropdown\">\n          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>\n          <ul class=\"dropdown-menu\">\n            <li><a href=\"#\">Action</a></li>\n            <li><a href=\"#\">Another action</a></li>\n            <li><a href=\"#\">Something else here</a></li>\n            <li role=\"separator\" class=\"divider\"></li>\n            <li><a href=\"#\">Separated link</a></li>\n            <li role=\"separator\" class=\"divider\"></li>\n            <li><a href=\"#\">One more separated link</a></li>\n          </ul>\n        </li>\n      </ul>\n      <form class=\"navbar-form navbar-left\">\n        <div class=\"form-group\">\n          <input type=\"text\" class=\"form-control\" placeholder=\"Search\">\n        </div>\n        <button type=\"submit\" class=\"btn btn-default\">Submit</button>\n      </form>\n      <ul class=\"nav navbar-nav navbar-right\">\n        <li><a href=\"#\">Sign In</a></li>\n      </ul>\n    </div><!-- /.navbar-collapse -->\n  </div><!-- /.container-fluid -->\n</nav>\n";
 
 /***/ }),
 /* 56 */
